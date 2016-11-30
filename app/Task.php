84d6b55a2986046412e0a8c78cf43c0eb7554de0
James <jpaik456@gmail.com>
2016-11-30 21:00:40 +0000

    Create initial models

---
+++ app/Task.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    protected $fillable = [
+      'list_id', 'description', 'position', 'state', 'comment', 'color'
+    ];
+
+    public static $create_validation_rules = [
+      'description' => 'required'
+    ]
+}
