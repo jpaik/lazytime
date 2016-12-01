b3581412e749ab21b52bdc3b8d31f0c59a27cb9a
James <jpaik456@gmail.com>
2016-12-01 19:14:01 +0000

    Testing Sass and Gulp

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,46 +2,7 @@
 <html>
     <head>
         <title>Lazytime</title>
-
-        <link href="https://fonts.googleapis.com/css?family=Lato:100,300" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-                background-image:
-                  radial-gradient(circle at bottom right, #fff,#24C6DC 20%, #514A9D 90%);
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-                padding-bottom: 50px;
-                color: #fff;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-            .desc{
-              font-size:28px;
-              font-weight: 300;
-            }
-        </style>
+        <link href="css/app.css" rel="stylesheet" type="text/css">
     </head>
     <body>
         <div class="container">
@@ -52,5 +13,7 @@
                 </div>
             </div>
         </div>
+
+        <script src="js/library.js"></script>
     </body>
 </html>
