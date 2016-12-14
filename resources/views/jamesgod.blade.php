f9ba4179e5918579a256e0388ea7fe3458b41db5
James <jpaik456@gmail.com>
2016-12-14 14:50:17 +0000

    Test resource view

--- resources/views/jamesgod.blade.php
+++ resources/views/jamesgod.blade.php
@@ -6,5 +6,5 @@
     <div class="col-md-12 text-center">
         <h1><a href="http://jpaik.me" target="_blank">Jamesgod</a></h1>
         <h1><a href="http://davenwu.com" target="_blank">Davengod</a></h1>
-    </div>
+    </div
 @endsection
